7e675c5063d6b9c4ec9e5dff3b9e432e083f3d26
Barak Ben-David <bbendavi@cisco.com>
2018-01-31 14:13:43 -0800

    first commit

---
+++ src/App.ts
@@ -0,0 +1,56 @@
+//create express app
+// const express = require('express');
+// const app = express();
+
+/* DotEnv - Simulate Environment variables
+require dotenv for reading variables from local .env file
+if statement checks that we are not running on prod environment -
+in which case we'd read the actual evn using process.env.<VARIABLE_NAME>
+ */
+// if (process.env.NODE_ENV !== 'production') { 
+//     require('dotenv').load();
+// }
+//Server is only sending jsons so we only need body-parser json method
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+//require routes and store in variable in order to call and pass in express app
+/* require('./server/config/routes.js')(app);
+const routes = require('./server/config/routes.js');
+routes(app);
+ */
+// app.use(require('./server/config/routes'))
+
+// const port = process.env.PORT || 3000;
+//Start server
+// app.listen(port, function () {
+//     console.log(`listening on port ${port}`);
+// });
+// module.exports = app
+
+
+//Typescript
+import * as express from 'express'
+class App {
+  public express
+
+  constructor () {
+    this.express = express()
+    this.mountRoutes()
+  }
+
+  private mountRoutes (): void {
+    const router = express.Router()
+    router.get('/', (req, res) => {
+      res.json({
+        message: 'Hello World!'
+      })
+    })
+    this.express.use('/', router)
+  }
+}
+
+export default new App().express
