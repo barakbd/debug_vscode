517378792b2e1723886903915063e767b7c228de
Barak Ben-David <bbendavi@cisco.com>
2018-02-02 13:11:48 -0800

    only service account routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,27 +1,21 @@
-const boxManagedUserSDK = require("./box_managed_user");
-
-const express = require('express'),
-  router = express.Router()
-router.use('/box/linkCardFolder/:folderName', require(`../controllers/box/link_card_folder`)(boxManagedUserSDK))
-
-
-
 /************  SERVICE ACCOUNT METHODS *********************
 https://stackoverflow.com/questions/45023624/how-to-require-multiple-controllers-with-express
 https://www.terlici.com/2014/09/29/express-router.html
  */
 
+ const express = require('express'),
+  router = express.Router()
+
 const fs = require('fs'), path = require("path");
 const boxServiceAccountClient = require("./box_service_account");
 
  const boxControllers = fs.readdirSync(path.join(__dirname, "../controllers/box"))
-  .filter(f => f !== 'link_card_folder.js')
+  .filter(f => f !== 'index.js')
 
 // var storageControllers={};
 boxControllers.forEach(controller => {
     // const path = `../controllers/storage/${controller}`
     router.use('/box/serviceAccount', require(`../controllers/box/${controller}`)(boxServiceAccountClient))
 });
-
 
 module.exports = router
