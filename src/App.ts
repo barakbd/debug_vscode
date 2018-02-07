da678cfaec0f8d5d5cb8a95fba46d22b00578ad6
Barak Ben-David <bbendavi@cisco.com>
2018-02-06 16:40:48 -0800

    ty

--- src/App.ts
+++ src/App.ts
@@ -1,29 +1,3 @@
-//create express app
-// const express = require('express');
-// const app = express();
-
-//Server is only sending jsons so we only need body-parser json method
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-
-//require routes and store in variable in order to call and pass in express app
-/* require('./server/config/routes.js')(app);
-const routes = require('./server/config/routes.js');
-routes(app);
- */
-// app.use(require('./server/config/routes'))
-
-// const port = process.env.PORT || 3000;
-//Start server
-// app.listen(port, function () {
-//     console.log(`listening on port ${port}`);
-// });
-// module.exports = app
-
-//Typescript
 import * as express from "express";
 import * as bodyParser from "body-parser";
 // import routes from
--- src/server/controllers/box/folder.ts
+++ src/server/controllers/box/folder.ts
@@ -1,108 +1,97 @@
 import * as express from "express";
 const router: express.Router = express.Router();
 
-// export default 
-class folderMethods{
-    public boxClient: any;
-
-    constructor(boxClient:any) {
-      this.boxClient = boxClient;
-    } //end constructor
+export default function(boxClient: any) {
+    const folderMethods = new FolderMethods(boxClient);
+    router.post("/folder", folderMethods.create);
+    router.get("/folder/:id", folderMethods.get);
+    router.get("/folder/:id/items", folderMethods.getItems);
+    router.get("/folder/search/:folder_name", folderMethods.search);
   
-    public create(req: express.Request, res: express.Response) {
-        if (!req.body || !req.body.folder_name) {
-            return res.json("can't add empty folderName")
-        } else if (typeof req.body.folder_name !== "string") {
-            return res.json({
-                status: 400,
-                message: "folderName must be a string"
-            })
-
-    };//end create
-
-  
-}; //end class folderMethods
-
-const folderMethods = function(client: any){
-
-    const folderMethods = {
-        create: function(req: express.Request, res: express.Response) {
-            if (!req.body || !req.body.folder_name) {
-                return res.json("can't add empty folderName")
-            } else if (typeof req.body.folder_name !== "string") {
-                return res.json({
-                    status: 400,
-                    message: "folderName must be a string"
-                })
-            }
-        
-            client.folders.create(0, req.body.folder_name).then((folderInfo: any) => {
-                return res.json({
-                    status: 400,
-                    data: folderInfo
-                })
-            }).catch((err: any) => {
-                return res.json(err)
-            });
-        }, //end create
-        
-        get: function (req: express.Request, res: express.Response) {
-            client.folders.get(/* "45416054928" */req.params.id, {
-                fields: 'name,shared_link,permissions,collections,sync_state'
-            }).then((folderInfo: any) => {
-                return res.json({
-                    status: 200,
-                    data: folderInfo
-                })
-            }).catch((err:any) => {
-                return res.json(err)
-            })
-        }, //end get
-        
-        getItems: function (req: express.Request, res: express.Response) {
-            client.folders.getItems(/* "45416054928" */req.params.id, {
-                fields: 'name,shared_link,permissions,collections,sync_state'
-            }).then((folderInfo: any) => {
-                return res.json({
-                    status: 200,
-                    data: folderInfo
-                })
-            }).catch((err: any) => {
-                return res.json(err)
-            })
-        }, //end get
-
-
-        search: function (req: express.Request, res: express.Response) {
-            client.search.query(req.params.folder_name, 
-                {
-                    fields: 'name,modified_at,size,extension,permissions,sync_state, collections',
-                    type: 'folder',
-                    ancestor_folder_ids: 0,
-                    limit: 5,
-                    offset: 0,
-                }
-            ).then((folders: any[]) => {
-                return res.json({
-                    status: 200,
-                    data: folders
-                })
-            }).catch((err: any) => {
-                return res.json(err)
-            })
-        } //end search
-
-        
-    };//end folderMethods
-
-    const express = require('express'),
-    router = express.Router()
-
-    router.post("/folder", folderMethods.create)
-    router.get("/folder/:id", folderMethods.get)
-    router.get("/folder/:id/items", folderMethods.getItems)
-    router.get("/folder/search/:folder_name", folderMethods.search)
-
     return router;
-    
-};//end module.exports
+  }; //end folderRoutes
+  
+// export default
+export class FolderMethods {
+  public boxClient: any;
+
+  constructor(boxClient: any) {
+    this.boxClient = boxClient;
+  } //end constructor
+
+  public create(req: express.Request, res: express.Response) {
+    if (!req.body || !req.body.folder_name) {
+      return res.json("can't add empty folderName");
+    } else if (typeof req.body.folder_name !== "string") {
+      return res.json({
+        status: 400,
+        message: "folderName must be a string"
+      });
+    }
+
+    this.boxClient.folders
+      .create(0, req.body.folder_name)
+      .then((folderInfo: any) => {
+        return res.json({
+          status: 400,
+          data: folderInfo
+        });
+      })
+      .catch((err: any) => {
+        return res.json(err);
+      });
+  } //end create
+
+  public get(req: express.Request, res: express.Response) {
+    this.boxClient.folders
+      .get(/* "45416054928" */ req.params.id, {
+        fields: "name,shared_link,permissions,collections,sync_state"
+      })
+      .then((folderInfo: any) => {
+        return res.json({
+          status: 200,
+          data: folderInfo
+        });
+      })
+      .catch((err: any) => {
+        return res.json(err);
+      });
+  } //end get
+
+  public getItems(req: express.Request, res: express.Response) {
+    this.boxClient.folders
+      .getItems(/* "45416054928" */ req.params.id, {
+        fields: "name,shared_link,permissions,collections,sync_state"
+      })
+      .then((folderInfo: any) => {
+        return res.json({
+          status: 200,
+          data: folderInfo
+        });
+      })
+      .catch((err: any) => {
+        return res.json(err);
+      });
+  } //end getItems
+
+  public search(req: express.Request, res: express.Response) {
+    this.boxClient.search
+      .query(req.params.folder_name, {
+        fields:
+          "name,modified_at,size,extension,permissions,sync_state, collections",
+        type: "folder",
+        ancestor_folder_ids: 0,
+        limit: 5,
+        offset: 0
+      })
+      .then((folders: any[]) => {
+        return res.json({
+          status: 200,
+          data: folders
+        });
+      })
+      .catch((err: any) => {
+        return res.json(err);
+      });
+  } //end search
+} //end class FolderMethods
