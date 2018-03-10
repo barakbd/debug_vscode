1f9ab97e78c8546af25febc3412f79b97797e230
Barak Ben-David <bbendavi@cisco.com>
2018-03-09 16:32:52 -0800

    forward file working

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM node:8.9-alpine
 
-ENV NODE_ENV production
-WORKDIR /user
+WORKDIR /server
 
 # leverage layers by installing node_modules in a seperate step than copying the server files.
 # if no changes were made package.json - cache will be used - fatster builds
-COPY package.json package-lock.json* npm-shrinkwrap.json* src ./
+COPY package.json package-lock.json* npm-shrinkwrap.json* /server
 RUN npm i 
-RUN npm run build
 
 COPY ./dist ./
 # EXPOSE - informational ony
--- src/server/controllers/files.ts
+++ src/server/controllers/files.ts
@@ -1,3 +1,6 @@
+// https://github.com/box/box-node-sdk/blob/master/docs/files.md#upload-a-file
+// http://qnimate.com/stream-file-uploads-to-storage-server-in-node-js/
+
 import {
   Router,
   Request,
@@ -6,7 +9,8 @@ import {
   // RequestHandler
 } from "express";
 
-// import * as defaultExport from "../route-controller-module"
+import * as multiparty from "multiparty"
+
 const createRoutes: Function = (boxClient: any): Router => {
   const router: Router = Router();
 
@@ -25,15 +29,27 @@ class FileMethods {
     this._boxClientLocal = boxClient;
   } //end constructor
 
-  public create = (req: Request, res: Response): Response => {
-    return this._boxClientLocal.files
-      .create(req.body.ancestor_file_id, req.body.new_file_name)
-      .then((fileInfo: any) => {
-        return res.status(200).json(fileInfo);
-      })
-      .catch((err: any) => {
-        return res.status(err.statusCode).json(err);
-      });
+  public create = (req: Request, res: Response) => {
+    const multipartyForm: multiparty.Form = new multiparty.Form();
+    multipartyForm.on("part", (part: multiparty.Part)=>{
+      if (part.filename){
+        console.log(part);
+        return res.json()
+      }
+    })
+    multipartyForm.on("error", function(error) {
+      console.log(error);
+    });
+    //start parsing
+    multipartyForm.parse(req);
+    // return this._boxClientLocal.files
+    //   .uploadFile(req.body.file, req.body.new_file_name)
+    //   .then((fileInfo: any) => {
+    //     return res.status(200).json(fileInfo);
+    //   })
+    //   .catch((err: any) => {
+    //     return res.status(err.statusCode).json(err);
+    //   });
   }; //end create
 
   public get = (req: Request, res: Response) => {
