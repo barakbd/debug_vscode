3465c1c38fdf8c4b888632f2a2d474ed2b701e80
Barak Ben-David <bbendavi@cisco.com>
2018-03-13 18:05:36 -0700

    file works

--- src/server/controllers/files.ts
+++ src/server/controllers/files.ts
@@ -1,5 +1,4 @@
 // https://github.com/box/box-node-sdk/blob/master/docs/files.md#upload-a-file
-// http://qnimate.com/stream-file-uploads-to-storage-server-in-node-js/
 
 import {
   Router,
@@ -9,14 +8,17 @@ import {
   // RequestHandler
 } from "express";
 
-import * as multiparty from "multiparty"
+import * as multer from "multer";
+const upload = multer({ storage: multer.memoryStorage() });
+// When encountering an error, multer will delegate the error to express.
 
 const createRoutes: Function = (boxClient: any): Router => {
   const router: Router = Router();
 
   const fileMethods = new FileMethods(boxClient);
-  router.post("/", fileMethods.create);
-  router.get("/:id", fileMethods.get);
+  router.post("/:parent_folder_id", upload.single("file"), fileMethods.create);
+  router.get("/:box_file_id", fileMethods.get);
+  router.put("/getSharedLink/:box_file_id", fileMethods.getSharedLink);
   return router;
 };
 
@@ -30,31 +32,22 @@ class FileMethods {
   } //end constructor
 
   public create = (req: Request, res: Response) => {
-    const multipartyForm: multiparty.Form = new multiparty.Form();
-    multipartyForm.on("part", (part: multiparty.Part)=>{
-      if (part.filename){
-        console.log(part);
-        return res.json()
-      }
-    })
-    multipartyForm.on("error", function(error) {
-      console.log(error);
-    });
-    //start parsing
-    multipartyForm.parse(req);
-    // return this._boxClientLocal.files
-    //   .uploadFile(req.body.file, req.body.new_file_name)
-    //   .then((fileInfo: any) => {
-    //     return res.status(200).json(fileInfo);
-    //   })
-    //   .catch((err: any) => {
-    //     return res.status(err.statusCode).json(err);
-    //   });
+    const file: Express.Multer.File = req.file;
+    // const fileBuffer: Buffer = file.buffer; - informative
+
+    this._boxClientLocal.files
+      .uploadFile(req.params.parent_folder_id, file.originalname, file.buffer)
+      .then((file: any) => {
+        return res.status(200).json(file);
+      })
+      .catch((err: any) => {
+        return res.status(err.statusCode).json(err);
+      });
   }; //end create
 
   public get = (req: Request, res: Response) => {
     this._boxClientLocal.files
-      .get(req.params.id)
+      .get(req.params.box_file_id)
       .then((fileInfo: any) => {
         return res.status(200).json(fileInfo);
       })
@@ -63,4 +56,16 @@ class FileMethods {
       });
   }; //end get
 
+  public getSharedLink = (req: Request, res: Response) => {
+    this._boxClientLocal.files
+      .update(req.params.box_file_id, {
+        shared_link: this._boxClientLocal.accessLevels.DEFAULT,
+      })
+      .then((fileInfo: any) => {
+        return res.status(200).json(fileInfo);
+      })
+      .catch((err: any) => {
+        return res.status(err.statusCode).json(err);
+      });
+  }; //end get
 } //end class FileMethods
