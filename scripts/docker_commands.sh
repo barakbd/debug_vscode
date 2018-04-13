be12f19ab0baa8a5f11b6f00743b6e2069e646cf
Barak Ben-David <bbendavi@cisco.com>
2018-04-13 11:27:59 -0700

    gitignore and dockerignore

--- scripts/docker_commands.sh
+++ scripts/docker_commands.sh
@@ -1,14 +1,27 @@
-#  https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes
+
+########## ENV VARS ###############
 
 # open bash_profile in text editor - sudo open -e ~/.bash_profile
 # clear bash_history - cat /dev/null > ~/.bash_history && history -c && exit
 
+########## DOCKER BUILD AND PUSH ###############
+
 docker login containers.cisco.com
+-generate encrypted password
 
 # build
-docker build --no-cache -t cdt_box:local . 
-# run - start is a comman to pass to entrypoint
-docker run -it --rm -p 4000:4000 --env-file .env cdt_box:local start 
+docker build --no-cache -t cdt_box:dev . 
+# run cdt_box
+docker run -it --rm -p 4000:4000 cdt_box:dev start 
+# run mongo
+docker run --rm -it -v /data:/data -p 27018:27017 mongo:3.6
+docker-compose up
+docker-compose down - will rm containers 
+
+########## DOCKER CLEANUP ###############
+#  https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes
+
+########## CLEANUP IMAGES ###############
 
 # inspect image
 docker image inspect 65ab307748
@@ -23,14 +36,12 @@ docker rmi $(docker images -a -q)
 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
 
 
+########## CLEANUP CONTAINERS ###############
 
 docker container stop 1fa4ab2cf395
 
 docker tag cdt_box bbendavi/cdt_box:dev
 docker push bbendavi/cdt_box:dev
-
-# will pull if doesn't exist
-docker run --rm -p 4000:4000  --env-file .env bbendavi/cdt_box:local 
 
 # list containers
 docker ps -a
@@ -49,10 +60,3 @@ docker rm -f $(docker ps -a -q)
 http://phase2.github.io/devtools/common-tasks/ssh-into-a-container/
 # look inside running container
 docker exec -it 9adffb2a6394 ls
-
-
-# run mongo image 
-docker run -it --rm p
-
-# mongo
-docker run --rm -it -v /data:/data -p 27018:27017 mongo:3.6
